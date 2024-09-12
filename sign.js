d3bb8b10cfb70b669d7b6ee47a217add296186fa
JunHo <poiewq1731@gmail.com>
2024-09-12 15:45:05 +0900

    Create sign.js

---
+++ sign.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const { createHash } = require('crypto')
+const path = require('path')
+
+const run = js => {
+    const hashSha256 = createHash('sha256')
+    const input = fs.createReadStream(path.join(__dirname, '../', js))
+    input.on('readable', () => {
+        const data = input.read()
+        if (data) {
+            hashSha256.update(data)
+        } else {
+            const integrity = `sha256-${hashSha256.digest('base64')}`
+            console.log(`${js}: ${integrity}`)
+        }
+    })
+}
+
+[
+  'main.js',
+  'styling.js'
+].forEach(js => run(js))
